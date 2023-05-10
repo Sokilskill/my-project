0c7e7d8b2babdc72ed065c333cc640f644d7132b
Sokilskill <vprovorotov@gmail.com>
2023-05-10 21:55:36 +0300

    feature/headerscroll
    додано скролл доверху сторінки

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import * as swiper from '/js/swiper.js';
 import * as modal from '/js/modal.js';
 import * as mobile from '/js/mobile-menu.js';
+import * as scroll from '/js/scroll.js';
 
 // Використання модулів
 swiper.file1();
 swiper.file2();
 modal.file1();
 mobile.file1();
+scroll.file1();
+scroll.file2();
 // тощо
